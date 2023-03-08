cf7d3ac1bc0573141dfd24f8485526c4fe27c8c4
abdi-edoc-de <se.abdulfeta.dedgba@gmail.com>
2023-03-08 08:49:45 +0300

    allowed_hosts aded

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = "django-insecure-0!kxm9o79zqv3--^lr&_$c)@(bzk_jd6=l4_-)b#3zsd^l1)r$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'https://abdide.pythonanywhere.com'
+]
 
 
 # Application definition
